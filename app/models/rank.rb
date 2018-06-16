d9dcbf23cb1dd93590a9fd6cd3a2171e87a44842
Artem Russkikh <rusartx@gmail.com>
2018-06-16 09:44:19 +0500

    Add ranks

---
+++ app/models/rank.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: ranks
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Rank < ApplicationRecord
+end
---
+++ app/views/ranks/_rank.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! rank, :id, :title, :created_at, :updated_at
+json.url rank_url(rank, format: :json)
